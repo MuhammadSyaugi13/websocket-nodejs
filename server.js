4a4127e3317cd51673aa9f80762b7f7851e6dcda
MuhammadSyaugi13 <muhammadsyaugi13@gmail.com>
2024-06-29 12:59:28 +0700

    mengubah sedikit server.js

--- server.js
+++ server.js
@@ -14,52 +14,54 @@ app.use(BodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.set("views", "views")
 
-const db = mysql.createConnection({
-    host: 'localhost', 
-    database: 'school', 
-    user: 'root', 
-    password: '', 
+// const db = mysql.createConnection({
+//     host: 'localhost', 
+//     database: 'school', 
+//     user: 'root', 
+//     password: '', 
+// })
+
+// db.connect((err) => {
+//     if (err) throw err;
+
+//     console.log('database berhasil connect');
+    
+    
+//     // ambil
+//     app.get('/', (req, res)=>{
+//         const sql = 'select * from user'
+        
+//         db.query(sql, (err, result) => {
+//             // res.send(users)
+//             const users = JSON.parse(JSON.stringify(result))
+//             res.render("index", {users, title: 'DAFTAR MURID KELAS'},)
+//         })
+//     })
+    
+//     app.post('/tambah', (req, res)=>{
+//         const sqlInsert = `insert into user (nama, kelas) values ('${req.body.nama}', '${req.body.kelas}')`
+        
+//         // insert
+//         db.query(sqlInsert, (err, result)=> {
+//             if (err) {
+//                 console.log('error insert');
+//                 throw err
+//             }
+
+//             res.redirect('/')
+//         })
+
+//         // res.render("index", {users, title: 'DAFTAR MURID KELAS'},)
+//     })
+//     // console.log('hasil database -> ', result);
+
+// })
+
+
+app.get('/chat', (req, res)=>{
+    res.render("chat", {title: "masuk forum"})
 })
 
-db.connect((err) => {
-    if (err) throw err;
-
-    console.log('database berhasil connect');
-    
-    
-    // ambil
-    app.get('/', (req, res)=>{
-        const sql = 'select * from user'
-        
-        db.query(sql, (err, result) => {
-            // res.send(users)
-            const users = JSON.parse(JSON.stringify(result))
-            res.render("index", {users, title: 'DAFTAR MURID KELAS'},)
-        })
-    })
-    
-    app.get('/chat', (req, res)=>{
-        res.render("chat", {title: "masuk forum"})
-    })
-
-    app.post('/tambah', (req, res)=>{
-        const sqlInsert = `insert into user (nama, kelas) values ('${req.body.nama}', '${req.body.kelas}')`
-        
-        // insert
-        db.query(sqlInsert, (err, result)=> {
-            if (err) {
-                console.log('error insert');
-                throw err
-            }
-
-            res.redirect('/')
-        })
-
-        // res.render("index", {users, title: 'DAFTAR MURID KELAS'},)
-    })
-    // console.log('hasil database -> ', result);
-
-})
 
 io.on('connection', (socket)=>{
     socket.on("message", (data) => {
